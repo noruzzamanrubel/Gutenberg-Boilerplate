6a90469cf61f462c0b283eabd28df925a3a37e8f
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-09-08 14:22:31 +0600

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c64471f99f971ab7011b');
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { registerBlockType } from '@wordpress/blocks';
+import './style.scss';
+
+/**
+ * Internal dependencies
+ */
+import metadata from './block.json';
+import Edit from './edit';
+import save from './save';
+
+registerBlockType( metadata.name, {
+	edit: Edit,
+	save,
+} );
